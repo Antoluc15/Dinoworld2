a911d011d25f54bc9c989c748e7bc1ea38f3f862
Antoluc15 <144289430+Antoluc15@users.noreply.github.com>
2024-11-14 23:37:45 -0600

    Update api.php

--- api.php
+++ api.php
@@ -4,6 +4,10 @@ header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
 
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
